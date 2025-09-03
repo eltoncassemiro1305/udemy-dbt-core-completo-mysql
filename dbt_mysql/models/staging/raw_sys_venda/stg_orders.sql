2a5a6230c899c5f08c7102dd9b3558b94cca4c00
Jornada de Dados <elton249@gmail.com>
2025-09-03 20:14:34 -0300

    etapa 3: implementação da view  no projeto dbt

---
+++ dbt_mysql/models/staging/raw_sys_venda/stg_orders.sql
@@ -0,0 +1,23 @@
+
+{{ 
+    config(
+        tags=['comercial']
+    ) 
+}}
+
+with orders as (
+    select
+        orderNumber,
+        orderDate,
+        requiredDate,
+        shippedDate,
+        status,
+        comments,
+        customerNumber
+    from {{ source('sys_venda','orders') }}
+
+)
+
+select
+  *
+from orders
